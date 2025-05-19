1bc48f3d495232b8ea311089b072d84a302f3291
Z <johncarlo_villaceran@yahoo.com>
2025-05-19 11:24:40 +0800

    Login page and header initial stylings

--- blocks/commerce-login/commerce-login.js
+++ blocks/commerce-login/commerce-login.js
@@ -5,6 +5,7 @@ import { render as authRenderer } from '@dropins/storefront-auth/render.js';
 import { checkIsAuthenticated } from '../../scripts/configs.js';
 import { CUSTOMER_FORGOTPASSWORD_PATH, CUSTOMER_ACCOUNT_PATH } from '../../scripts/constants.js';
 import { rootLink } from '../../scripts/scripts.js';
+import { events } from '@dropins/tools/event-bus.js';
 
 // Initialize
 import '../../scripts/initializers/auth.js';
@@ -17,5 +18,42 @@ export default async function decorate(block) {
       routeForgotPassword: () => rootLink(CUSTOMER_FORGOTPASSWORD_PATH),
       routeRedirectOnSignIn: () => rootLink(CUSTOMER_ACCOUNT_PATH),
     })(block);
+
+    /**
+     * add custom text
+     */
+    const loginTextContainer = block.querySelector('.dropin-header-container.auth-sign-in-form__title');
+    if (loginTextContainer) {
+      const loginText = document.createElement('p');
+      loginText.innerText = 'If you have an account, sign in with your email address.';
+      loginText.classList.add('login-text');
+      loginTextContainer.after(loginText);
+    }
+
+    events.on(
+      'aem/lcp',
+      () => {
+        /**
+         * Hide show/hide password
+         */
+        const $eye = document.querySelector('.dropin-input-password__eye-icon');
+        $eye.style.display = 'none';
+
+        /**
+         * add label for email and password
+         */
+        const $email = document.querySelector('.auth-sign-in-form__form__field--email .dropin-input-label-container');
+        const $password = document.querySelector('.auth-sign-in-form__form__password .dropin-input-label-container');
+
+        const emailLabel = document.createElement('p');
+        emailLabel.innerText = 'Email.';
+        $email.prepend(emailLabel);
+
+        const passwordLabel = document.createElement('p');
+        passwordLabel.innerText = 'Password.';
+        $password.prepend(passwordLabel);
+      },
+      { eager: true },
+    );
   }
 }
--- blocks/header/header.js
+++ blocks/header/header.js
@@ -217,6 +217,60 @@ export default async function decorate(block) {
 
   const navTools = nav.querySelector('.nav-tools');
 
+  /** Search */
+  // TODO
+  const search = document.createRange().createContextualFragment(`
+  <div class="search-wrapper nav-tools-wrapper">
+    <button type="button" class="nav-search-button">Search</button>
+    <div class="nav-search-input nav-search-panel nav-tools-panel">
+      <form action="/search" method="GET">
+        <input id="search" type="search" name="q" placeholder="Search" />
+        <div id="search_autocomplete" class="search-autocomplete"></div>
+      </form>
+    </div>
+  </div>
+  `);
+
+  navTools.append(search);
+
+  const searchPanel = navTools.querySelector('.nav-search-panel');
+
+  const searchButton = navTools.querySelector('.nav-search-button');
+
+  const searchInput = searchPanel.querySelector('input');
+
+  const searchForm = searchPanel.querySelector('form');
+
+  searchForm.action = rootLink('/search');
+
+  async function toggleSearch(state) {
+    const show = state ?? !searchPanel.classList.contains('nav-tools-panel--show');
+
+    searchPanel.classList.toggle('nav-tools-panel--show', show);
+
+    if (show) {
+      await import('./searchbar.js');
+      searchInput.focus();
+    }
+  }
+
+  navTools.querySelector('.nav-search-button').addEventListener('click', () => {
+    if (isDesktop.matches) {
+      toggleAllNavSections(navSections);
+      overlay.classList.remove('show');
+    }
+    toggleSearch();
+  });
+
+  /**
+   * Customer login
+   */
+  renderAuthCombine(
+    navSections,
+    () => !isDesktop.matches && toggleMenu(nav, navSections, false),
+  );
+  renderAuthDropdown(navTools);
+
   /** Mini Cart */
   const excludeMiniCartFromPaths = ['/checkout'];
 
@@ -269,51 +323,6 @@ export default async function decorate(block) {
     { eager: true },
   );
 
-  /** Search */
-  // TODO
-  const search = document.createRange().createContextualFragment(`
-  <div class="search-wrapper nav-tools-wrapper">
-    <button type="button" class="nav-search-button">Search</button>
-    <div class="nav-search-input nav-search-panel nav-tools-panel">
-      <form action="/search" method="GET">
-        <input id="search" type="search" name="q" placeholder="Search" />
-        <div id="search_autocomplete" class="search-autocomplete"></div>
-      </form>
-    </div>
-  </div>
-  `);
-
-  navTools.append(search);
-
-  const searchPanel = navTools.querySelector('.nav-search-panel');
-
-  const searchButton = navTools.querySelector('.nav-search-button');
-
-  const searchInput = searchPanel.querySelector('input');
-
-  const searchForm = searchPanel.querySelector('form');
-
-  searchForm.action = rootLink('/search');
-
-  async function toggleSearch(state) {
-    const show = state ?? !searchPanel.classList.contains('nav-tools-panel--show');
-
-    searchPanel.classList.toggle('nav-tools-panel--show', show);
-
-    if (show) {
-      await import('./searchbar.js');
-      searchInput.focus();
-    }
-  }
-
-  navTools.querySelector('.nav-search-button').addEventListener('click', () => {
-    if (isDesktop.matches) {
-      toggleAllNavSections(navSections);
-      overlay.classList.remove('show');
-    }
-    toggleSearch();
-  });
-
   // Close panels when clicking outside
   document.addEventListener('click', (e) => {
     if (!minicartPanel.contains(e.target) && !cartButton.contains(e.target)) {
@@ -359,10 +368,4 @@ export default async function decorate(block) {
   // prevent mobile nav behavior on window resize
   toggleMenu(nav, navSections, isDesktop.matches);
   isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
-
-  renderAuthCombine(
-    navSections,
-    () => !isDesktop.matches && toggleMenu(nav, navSections, false),
-  );
-  renderAuthDropdown(navTools);
 }
